0db52b6408b198b19a82dbd2e7c4a9ebb649c2f4
Oleg_Konichenko <Oleg_Konichenko@epam.com>
2016-12-12 17:48:26 +0200

    set limits

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -32,24 +34,34 @@ public class MainActivity extends AppCompatActivity {
      * @param quantity is the number of cups of coffee ordered
      */
     private int calculatePrice(int quantity) {
-        return quantity * 5;
+        return quantity * (5 + addChocolate() + addWhippedCream());
     }
 
     private String createOrderSummary(int quantity) {
-        return "Name: Oleg Konichenko\n"
-                + "Add whipped cream? " + addWhippedCream() +"\n"
-                + "Add chocolate? " + addChocolate() +"\n"
+        return "Name: " + getName() + "\n"
+                + "Whipped cream: $" + addWhippedCream() +"\n"
+                + "Chocolate: $" + addChocolate() +"\n"
                 + "Quantity: " + quantity + "\n"
                 + "Total: $" + calculatePrice(quantity) + "\n"
                 + "Thank you!";
     }
 
     public void increment(View view) {
+        if(quantity == 100) {
+            Toast.makeText(this, "You can't order more than 100 cups of coffee", Toast.LENGTH_SHORT)
+            .show();
+            return;
+        }
         quantity++;
         display(quantity);
     }
 
     public void decrement(View view) {
+        if(quantity == 1) {
+            Toast.makeText(this, "You can't order less than 1 cup of coffee", Toast.LENGTH_SHORT)
+            .show();
+            return;
+        }
         quantity--;
         display(quantity);
     }
@@ -62,14 +74,19 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    private boolean addWhippedCream() {
+    private int addWhippedCream() {
         CheckBox whippedCream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        return whippedCream.isChecked();
+        return whippedCream.isChecked() ? 1 : 0;
     }
 
-    private boolean addChocolate() {
+    private int addChocolate() {
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        return chocolate.isChecked();
+        return chocolate.isChecked() ? 2 : 0;
+    }
+
+    private String getName() {
+        EditText name = (EditText) findViewById(R.id.name_input);
+        return name.getText().toString();
     }
 
     /**
